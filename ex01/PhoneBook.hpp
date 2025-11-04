ffbb4c06c9dd64669ba9fabf1121e119b324b12d
Imadlhannouni <lhannouni05@gmail.com>
2025-11-04 15:36:15 +0100

    fixing some issues

--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: ilhannou <ilhannou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/02 19:08:23 by ilhannou          #+#    #+#             */
-/*   Updated: 2025/11/02 23:45:53 by ilhannou         ###   ########.fr       */
+/*   Updated: 2025/11/03 21:31:06 by ilhannou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,9 @@ class PhoneBook
 		Contact contact[8];
 		int index;
 	public:
-		PhoneBook() : index(0) {}
-
+		PhoneBook() {
+			index = 0;
+		}
 		void addContact(std::string firstn, std::string lastn, std::string nickn, std::string phone, std::string secret);
 		void showAll();
 		void showContact(int indx) const;
